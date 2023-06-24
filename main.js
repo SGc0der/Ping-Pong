0c58ed0ec0780c6fe7a9c43ea8507e2de6acc9f6
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-06-24 10:28:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 function setup() {
     canvas = createCanvas(540, 300);
     canvas.parent("canvas");
+    video = createCapture(VIDEO);
+    video.size(540, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+function draw() {
+    image(video, 0, 0);
+}
+function modelLoaded() {
+    console.log("model loaded");
 }

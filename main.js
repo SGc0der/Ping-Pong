b1ce666dadb1945befddf3de933b79721a3eaf87
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-08-02 18:40:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+rWristX = 0;
+rWristY = 0;
+rWristScore = 0;
 function setup() {
-    canvas = createCanvas(540, 300);
+    canvas = createCanvas(800, 600);
     canvas.parent("canvas");
     video = createCapture(VIDEO);
-    video.size(540, 300);
+    video.size(800, 600);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 function draw() {
-    image(video, 0, 0);
+    image(video, 0, 0, 800, 600);
+    if(rWristScore >= 0.2){
+        fill("red");
+        stroke("red");
+        circle(rWristX, rWristY, 50);
+    }
 }
 function modelLoaded() {
     console.log("model loaded");
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        rWristX = results[0].pose.rightWrist.x;
+        rWristY = results[0].pose.rightWrist.y;
+        rWristScore = results[0].pose.keypoints[10].score;
+        console.log(results);
+    } 
 }
